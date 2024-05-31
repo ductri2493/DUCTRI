98c219e5f98ed6d9e64cc1430edd472c4202ea3b
ductri2493 <tranductri632@gmail.com>
2024-05-31 15:07:34 +0700

    update

--- FER202/src/assets/components/lab/OrchidProduct.jsx
+++ FER202/src/assets/components/lab/OrchidProduct.jsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { OrchidsData } from "./ListOfOrchids";
-
+import { OrchidData } from "./ListOfOrchids";
 const OrchidProduct = () => {
   const userName = useParams();
-  const orchid = OrchidsData.find((obj) => {
-    return obj.id == userName.id;
+  const orchid = OrchidData.find((obj) => {
+    return obj.id === userName.id;
   });
-//   let cost = orchid.cost.toLocaleString();
+  //   let cost = orchid.cost.toLocaleString();
   return (
     <div className="min-h-[800px] bg-[#DADADA]">
       <div className="container top-10 pt-10 ">
         <div className="row bg-white p-10 min-h-[500px]">
           <div className="col-4">
-            <img src={`../${orchid.image}`} />
+            <img src={`../${orchid.Image}`} />
           </div>
           <div className="col-8">
-            <h1 className="fw-bold fs-3">{orchid.name}</h1>
-            <p className="mt-3">{orchid.description}</p>
-            
+            <h1 className="fw-bold fs-3">{orchid.Name}</h1>
+            <p className="mt-3">{orchid.Infor}</p>
           </div>
         </div>
       </div>
--- FER202/src/assets/components/lab/OrchidsContent.jsx
+++ FER202/src/assets/components/lab/OrchidsContent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { OrchidsData } from "./ListOfOrchids"; // Assumes you have an exported list of orchids from ListOfOrchids.js
+import { OrchidData } from "./ListOfOrchids";
 import Starrate from "../../images/starrate.png";
 import "./OrchidContent.css";
 import { Link } from "react-router-dom";
@@ -11,19 +11,19 @@ const OrchidsContent = () => {
     <div className="min-h-[1000px] bg-[#DADADA]">
       <div className="container top-10 pt-10">
         <div className="row align-items-start">
-          {OrchidsData.map((orchid) => (
-            <div className="col-3 mb-4" key={orchid.id}>
+          {OrchidData.map((orchid) => (
+            <div className="col-3 mb-4" key={orchid.Id}>
               <div className="card h-full">
                 <img
-                  src={orchid.image}
+                  src={orchid.Image}
                   className="card-img-top"
-                  alt={orchid.name}
+                  alt={orchid.Name}
                 />
                 <div className="card-body">
-                  <h4 className="card-title fw-bold">{orchid.name}</h4>
+                  <h4 className="card-title fw-bold">{orchid.Name}</h4>
                   <p className="card-text ">
-                    Rating: {orchid.rating}
-                    {[...Array(orchid.rating)].map((_, i) => (
+                    Rating: {orchid.Rating}
+                    {[...Array(orchid.Rating)].map((_, i) => (
                       <img
                         key={i}
                         src={Starrate}
@@ -33,24 +33,24 @@ const OrchidsContent = () => {
                       />
                     ))}
                     <br />
-                    Color: {orchid.color}
+                    Color: {orchid.Color}
                     <br />
-                    Origin: {orchid.origin}
+                    Origin: {orchid.Origin}
                     <br />
-                    Category: {orchid.category}
+                    Category: {orchid.Category}
                     <br />
                   </p>
-                  <Link to={`detail/${orchid.id}`}>
+                  <Link to={`detail/${orchid.Id}`}>
                     <button
                       onClick={() => {
                         setOrchid(orchid);
                       }}
                       type="button"
-                      className="btn btn-outline-warning mt-2 position-relative"
+                      className="btn btn-outline-warning mt-4 position-relative"
                     >
                       <a href="#popup1" id="openPopUp">
                         View Details
-                        {orchid.isSpecial && (
+                        {orchid.IsSpecial && (
                           <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                             Special
                           </span>
@@ -65,12 +65,12 @@ const OrchidsContent = () => {
         </div>
         <div id="popup1" className="overlay">
           <div className="popup">
-            <img src={orchid.image} />
-            <h2>{orchid.name}</h2>
+            <img src={orchid.Image} />
+            <h2>{orchid.Name}</h2>
             <a href="#" className="close">
               &times;
             </a>
-            <div className="content">{orchid.description}</div>
+            <div className="content">{orchid.Infor}</div>
           </div>
         </div>
       </div>
